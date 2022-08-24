e25c2b14211378c19029fe600e4e6a4c73757287
Tân Lê <tanvoyager@gmail.com>
2022-08-24 13:16:47 +0700

    admin

--- pages/admin/[id].jsx
+++ pages/admin/[id].jsx
@@ -1,12 +1,16 @@
+import { useRouter } from "next/router";
 import React from "react";
-import axios from "axios";
+import useSWR from "swr";
+import { BASE_URL, fetcher } from "../../api/getMethod";
 import DataTable from "../../components/Admin/DataTable";
 import Layout from "../../components/Admin/Layout";
 import Sidebar from "../../components/Admin/Sidebar";
-import useSWR from "swr";
 
-const Container = ({ item }) => {
-  console.log(item);
+const Container = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, error } = useSWR(BASE_URL + id, fetcher);
+
   return (
     <div className="flex min-h-screen bg-fb">
       <Sidebar />
@@ -18,27 +22,13 @@ const Container = ({ item }) => {
             image="IMAGE"
             price="PRICE"
             color="COLOR"
+            datas={data}
             size="SIZE"
-            
           />
         </Layout>
       </dir>
     </div>
   );
 };
-
-// export const getServerSideProps = async ({ params: { id } }) => {
-//   const res = await axios.get(
-//     `https://63030a4dc6dda4f287c1d8d4.mockapi.io/${id}`
-//   );
-//   return {
-//     props: {
-//       item: res.data,
-//     },
-//   };
-// };
-
-
-
 
 export default Container;
---
+++ pages/test/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SortTable from '../../test/SortTable'
+
+const index = () => {
+  return (
+    <div>
+        <SortTable/>
+    </div>
+  )
+}
+
+export default index
