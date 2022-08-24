f896a5724d0f06b315d8b5ce6d1c985a3f45882c
Tân Lê <tanvoyager@gmail.com>
2022-08-24 08:49:09 +0700

    table UI done

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Dashboard from '../../components/Admin/Dashboard'
 import Navbar from '../../components/Admin/Navbar'
 import Sidebar from '../../components/Admin/Sidebar'
 import DataTable from '../../components/Admin/DataTable'
+import Demo from '../../components/Admin/Demo';
 const Admin = () => {
+      const [isProduct,setProduct] = useState(true)
+
       return (
             <div className='flex h-screen w-full bg-fb'>
-                  <Sidebar />
+                  <Sidebar/>
                   
                   <div className='flex flex-col w-full'>
                         <Navbar />
                         <div className='w-full h-full bg-fb p-8'>
-                              <DataTable product='Product' name='NAME' image='IMAGE' price='PRICE' color='COLOR' size='SIZE'/>
+                              <Demo/>
                         </div>
                   </div>
             </div>
