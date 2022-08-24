6d5f0636e8918baa6c50e3a8b204b4baffa4bc6a
mindylonely <milulou2057@gmail.com>
2022-08-24 13:30:14 +0700

    add next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images:{
-    domains:['picsum.photos','loremflickr.com']
-  }
+      reactStrictMode: true,
+      swcMinify: true,
+      images: {
+            domains: ['picsum.photos', 'loremflickr.com']
+      }
 }
 
 module.exports = nextConfig
