40d4cd4daf5d96c4db61e9bd334c50e8b6ddd2b6
NgocHai <ngchai2410@gmail.com>
2022-08-24 10:48:27 +0700

    done task slider

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['assets.adidas.com', 'images.unsplash.com',]
+  }
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,57 @@
-import { Box } from "@mui/material";
-import { Container } from '@mui/material';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
+import { Box, Container } from "@mui/material";
 import Head from "next/head";
-import ProductCard from "../components/ProductCard/ProductCard";
+import React from 'react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import ProductCard from "../components/ProductCard";
+import Slider from '../components/Slider';
 
 export default function Home() {
+  const arrBlogs = [
+    {
+      id: 1,
+      title: 'mountain',
+      url: 'https://assets.adidas.com/images/w_766,h_766,f_auto,q_auto,fl_lossy,c_fill,g_auto/fde4287a315a464d915bad25006ec160_9366/%C3%A1o-thun-ba-l%C3%A1-essentials-adicolor-loungewear.jpg',
+
+      desc: 'i love nature'
+    },
+    {
+      id: 2,
+      title: 'waterfall',
+      url: 'https://assets.adidas.com/images/w_766,h_766,f_auto,q_auto,fl_lossy,c_fill,g_auto/5232fa5d0a1e4a1c92d1aed8008f493f_9366/%C3%A1o-thun-class-of-72-adidas-originals-unisex.jpg',
+
+      desc: 'i love water'
+    },
+    {
+      id: 3,
+      title: 'forest',
+      url: 'https://assets.adidas.com/images/w_766,h_766,f_auto,q_auto,fl_lossy,c_fill,g_auto/fde4287a315a464d915bad25006ec160_9366/%C3%A1o-thun-ba-l%C3%A1-essentials-adicolor-loungewear.jpg',
+
+      desc: 'i love forest'
+    },
+    {
+      id: 4,
+      title: 'sea',
+      url: 'https://assets.adidas.com/images/w_766,h_766,f_auto,q_auto,fl_lossy,c_fill,g_auto/fde4287a315a464d915bad25006ec160_9366/%C3%A1o-thun-ba-l%C3%A1-essentials-adicolor-loungewear.jpg',
+      desc: 'i love sea'
+    },
+    {
+      id: 5,
+      title: 'sea',
+      url: 'https://assets.adidas.com/images/w_766,h_766,f_auto,q_auto,fl_lossy,c_fill,g_auto/fde4287a315a464d915bad25006ec160_9366/%C3%A1o-thun-ba-l%C3%A1-essentials-adicolor-loungewear.jpg',
+      desc: 'i love sea'
+    },
+    {
+      id: 6,
+      title: 'sea',
+      url: 'https://assets.adidas.com/images/w_766,h_766,f_auto,q_auto,fl_lossy,c_fill,g_auto/fde4287a315a464d915bad25006ec160_9366/%C3%A1o-thun-ba-l%C3%A1-essentials-adicolor-loungewear.jpg',
+      desc: 'i love sea'
+    },
+  ]
+
   return (
     <div>
       <Head>
@@ -12,16 +60,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className="bg-red-300">Hello World</h1>
-      <Container className="product__slider">
-        <Box className="flex flex-col">
-          <span>Hàng mới về</span>
-          <span>Tinh hoa adidas</span>
-          <span>Sắp có</span>
+      {/* <Container className="product__slider">
+        <Box className="flex border-y-2 mb-4">
+          <span className='p-2 cursor-pointer'>Hàng mới về</span>
+          <span className='p-2 cursor-pointer'>Tinh hoa adidas</span>
+          <span className='p-2 cursor-pointer border-b-2 border-b-black'>Sắp có</span>
         </Box>
-        <div>
-          <ProductCard />
-        </div>
-      </Container>
+        <Slider arrProduct={arrBlogs} />
+      </Container> */}
+
+      <Slider arrProduct={arrBlogs} />
+
     </div>
   );
 }
