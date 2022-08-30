ce796d5a1fc4ff6e2b98a88c6d849f84f9e846b9
Tân Lê <tanvoyager@gmail.com>
2022-08-30 08:41:35 +0700

    add pagination

--- components/Admin/DataTable.jsx
+++ components/Admin/DataTable.jsx
@@ -12,6 +12,7 @@ import Image from "next/image";
 import Search from "./Search";
 import Box from "@mui/material/Box";
 import TablePagination from "@mui/material/TablePagination";
+import ReactFacebookLogin from "react-facebook-login";
 const DataTable = ({ type, datas }) => {
   const [query, setQuery] = useState("");
   const [searchResult, setSearchResult] = useState(datas);
@@ -161,6 +162,33 @@ const DataTable = ({ type, datas }) => {
     console.log(rowsPerPage)
 
   };
+  // SORT TABLE
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+  
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - createTableRows(datas).length) : 0;
 
@@ -221,6 +249,7 @@ const DataTable = ({ type, datas }) => {
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
+      
     </Box>
   );
 };
---
+++ pages/auth/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../../components/auth'
+
+const index = () => {
+  return (
+    <div>
+        <Login/>
+    </div>
+  )
+}
+
+export default index
